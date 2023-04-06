74015b0e1994cc146cb62b033124ac0d1689992e
Musky89 <irfaanameer@Irfaans-MacBook-Air.local>
2023-04-06 04:45:34 +0200

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./App.css";
+import './App.css';
 import Header from './components/Header';
 import HomePage from './components/pages/HomePage';
 import MainComponent from './components/MainComponent';
@@ -9,21 +9,29 @@ import AIDesignAssistantPanel from './components/AIDesignAssistantPanel';
 import DesignInputOutput from './components/DesignInputOutput';
 import OnboardingTutorial from './components/OnboardingTutorial';
 import ProjectManagement from './components/ProjectManagement';
-import Collaboration from "./components/Collaboration/Collaboration";
+import Collaboration from './components/Collaboration/Collaboration';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+
+const isLoggedIn = true; // Change this value to test access to the dashboard
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomePage />
-      <UserDashboard />
-      <ProjectManagement />
-      <Collaboration />
-      <DesignInterface />
-      <AIDesignAssistantPanel />
-      <DesignInputOutput />
-      <OnboardingTutorial />
-      <MainComponent />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/onboarding" element={<OnboardingTutorial />} />
+          <Route path="/project-management" element={<ProjectManagement />} />
+          <Route path="/collaboration" element={<Collaboration />} />
+          <Route path="/design-interface" element={<DesignInterface />} />
+          <Route path="/ai-design-assistant" element={<AIDesignAssistantPanel />} />
+          <Route path="/design-input-output" element={<DesignInputOutput />} />
+          <PrivateRoute path="/dashboard" isLoggedIn={isLoggedIn} element={<UserDashboard />} />
+        </Routes>
+        <MainComponent />
+      </Router>
     </div>
   );
 }
